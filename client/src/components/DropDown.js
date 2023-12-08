cd6ae9a5a94c3ff08d33c460f10f7698d40630b9
saesak <matthewcho2000@gmail.com>
2023-12-08 14:25:35 -0500

    added dropdown and changed website title and icon

---
+++ client/src/components/DropDown.js
@@ -0,0 +1,34 @@
+import React, { useState } from 'react';
+import { FormControl, InputLabel, Select, MenuItem } from '@mui/material';
+
+function DropDown({ options, label, onSelect }) {
+  const [selectedOption, setSelectedOption] = useState('');
+
+  const handleChange = (event) => {
+    const value = event.target.value;
+    setSelectedOption(value);
+    onSelect(value);
+  };
+
+  return (
+    <FormControl variant="outlined" fullWidth>
+      <InputLabel>{label}</InputLabel>
+      <Select
+        value={selectedOption}
+        onChange={handleChange}
+        label={label}
+      >
+        <MenuItem value="">
+          <em>None</em>
+        </MenuItem>
+        {options.map((option, index) => (
+          <MenuItem key={index} value={option}>
+            {option}
+          </MenuItem>
+        ))}
+      </Select>
+    </FormControl>
+  );
+}
+
+export default DropDown;
--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -3,10 +3,44 @@ import { Container, Divider, Link } from '@mui/material';
 import { NavLink } from 'react-router-dom';
 import leagueImg from '../images/league.ico';
 import backgroundImg from '../images/summoners_rift.png';
+import DropDown from '../components/DropDown';
+
 
 const config = require('../config.json');
 
+
 export default function HomePage() {
+  const [options, setOptions] = useState(['default']);
+  const [selectedOption, setSelectedOption] = useState('');
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch('./riot_champion.json');
+        
+        if (!response.ok) {
+          throw new Error(`Failed to load JSON file: ${response.status} ${response.statusText}`);
+        }
+
+        const data = await response.json();
+        const namesArray = data.map(item => item.id);
+
+        setOptions(namesArray);
+        console.log(namesArray);
+      } catch (error) {
+        console.error('Error:', error.message);
+      }
+    };
+
+    fetchData();
+  }, []); // Empty dependency array ensures that this effect runs once, similar to componentDidMount
+
+
+  const handleSelect = (value) => {
+    setSelectedOption(value);
+    // Do something with the selected value
+  };
+
   return (
     <div style={{ 
       backgroundImage: `url(${backgroundImg})`,
@@ -15,6 +49,8 @@ export default function HomePage() {
     }}>
       <div style={styles.overlay}>
         <div style={styles.container}>
+        <DropDown options={options} label="Select an option" onSelect={handleSelect} />
+        <p>Selected Option: {selectedOption}</p>
         <h1 style={{color:'white'}}>Welcome to League Simulator!</h1>
           <img src={leagueImg} alt="Description" style={styles.leagueIcon} />
         </div>
