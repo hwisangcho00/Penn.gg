962d0322d85a8325b90e99bbda4115f676401a5c
jinseo0904 <jinseokim@gimjinseoui-MacBookPro.local>
2023-12-12 17:42:38 -0500

    loading effects

--- client/src/pages/StatsPage.js
+++ client/src/pages/StatsPage.js
@@ -18,11 +18,7 @@ export default function StatsPage() {
     <div style={{
       backgroundImage: `url(${backgroundImg})`,
       backgroundRepeat: 'repeat',
-<<<<<<< HEAD
       backgroundSize: 'cover', // Or specify a size like '100px 100px'
-=======
-      backgroundSize: '1536px 864px', // Or specify a size like '100px 100px'
->>>>>>> 5ff0931 (UI updates)
       minHeight: '100vh',
       width: '100%', // Ensure the container spans the full width
       // If you need to ensure the container expands with its content:
